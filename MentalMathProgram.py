6415ae9428d266584354be8695435c1f69e28101
parosomniac <parosomniac@gmail.com>
2023-12-14 18:12:22 -0600

    create window 500x500 pixels

--- MentalMathProgram.py
+++ MentalMathProgram.py
@@ -1,5 +1,20 @@
-
 # Mental Math Program
 # graphical program to perform addition, subtraction, 
 # multiplication, division and keep track of time taken to solve
 # 13 December 2023
+
+import tkinter as tk
+
+root = tk.Tk()
+
+root.geometry("500x500")
+root.mainloop()
+
+# menu
+# digits of 1st num, digits of 2nd num
+
+# addition
+# subtraction
+# multiplication
+# division
+
