552c3384a2be7f965077d9f7ab54c880bc3d4c28
parosomniac <parosomniac@gmail.com>
2023-12-14 19:04:07 -0600

    file menu option to close

--- MentalMathProgram.py
+++ MentalMathProgram.py
@@ -11,6 +11,14 @@ class MyGUI:
     def __init__(self):
         self.root = tk.Tk()
 
+        self.menubar = tk.Menu(self.root)
+        self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label = "Close", command=exit)
+
+        self.menubar.add_cascade(menu=self.filemenu, label="File")
+
+        self.root.config(menu=self.menubar)
+
         self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
         self.label.pack(padx=10, pady=10)
 
