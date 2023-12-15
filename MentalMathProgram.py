c0a5c5e492742d8ebfc481c8ae0b65e44b9f7700
parosomniac <parosomniac@gmail.com>
2023-12-14 18:44:09 -0600

    clicking button sends message to console

--- MentalMathProgram.py
+++ MentalMathProgram.py
@@ -5,56 +5,31 @@
 
 import tkinter as tk
 
-root = tk.Tk()
+class MyGUI:
 
-root.geometry("500x500")
-root.title("Mental Math Program")
+    def __init__(self):
+        self.root = tk.Tk()
 
-label = tk.Label(root, text="Hello World!", font=('Arial', 18))
-label.pack(padx=20, pady=20)
+        self.label = tk.Label(self.root, text="Your Message", font=('Arial', 18))
+        self.label.pack(padx=10, pady=10)
 
-textbox = tk.Text(root, height=3, font=('Arial', 16))
-textbox.pack()
+        self.textbox = tk.Text(self.root, height=5, font=('Arial', 16))
+        self.textbox.pack(padx=10, pady=10)
 
-myentry = tk.Entry(root)
-myentry.pack(padx=10, pady=10)
+        self.check_state = tk.IntVar()
 
-button = tk.Button(root, text="Click Me!", font=('Arial', 18))
-button.pack(padx=10, pady=10)
+        self.check = tk.Checkbutton(self.root, text="Show Messagebox", font=('Arial', 16), variable=self.check_state)
+        self.check.pack(padx=10, pady=10)
 
-# grid layout (like calculator)
-# 3 columns
+        self.button = tk.Button(self.root, text="Show Message", font=('Arial', 18), command=self.show_message)
+        self.button.pack(padx=10, pady=10)
+        
+        self.root.mainloop()
 
-buttonframe = tk.Frame(root)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.columnconfigure(2, weight=1)
+    def show_message(self):
+        print("Hello World")
 
-btn1 = tk.Button(buttonframe, text="1", font=('Arial, 18'))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
-
-btn2 = tk.Button(buttonframe, text="2", font=('Arial, 18'))
-btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
-
-btn3 = tk.Button(buttonframe, text="3", font=('Arial, 18'))
-btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
-
-btn4 = tk.Button(buttonframe, text="4", font=('Arial, 18'))
-btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
-
-btn5 = tk.Button(buttonframe, text="5", font=('Arial, 18'))
-btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
-
-btn6 = tk.Button(buttonframe, text="6", font=('Arial, 18'))
-btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
-
-buttonframe.pack(fill='x')
-
-anotherbtn = tk.Button(root, text="Test")
-anotherbtn.place(x=200, y=200, height=100, width=100)
-
-
-root.mainloop()
+MyGUI()
 
 # menu
 # digits of 1st num, digits of 2nd num
