c0069bcfdc29ae57f679d99f31df61c3269a0534
parosomniac <parosomniac@gmail.com>
2023-12-14 18:28:38 -0600

    grid layout for buttons

--- MentalMathProgram.py
+++ MentalMathProgram.py
@@ -17,7 +17,38 @@ textbox = tk.Text(root, height=3, font=('Arial', 16))
 textbox.pack()
 
 myentry = tk.Entry(root)
-myentry.pack()
+myentry.pack(padx=10, pady=10)
+
+button = tk.Button(root, text="Click Me!", font=('Arial', 18))
+button.pack(padx=10, pady=10)
+
+# grid layout (like calculator)
+# 3 columns
+
+buttonframe = tk.Frame(root)
+buttonframe.columnconfigure(0, weight=1)
+buttonframe.columnconfigure(1, weight=1)
+buttonframe.columnconfigure(2, weight=1)
+
+btn1 = tk.Button(buttonframe, text="1", font=('Arial, 18'))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+
+btn2 = tk.Button(buttonframe, text="2", font=('Arial, 18'))
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+
+btn3 = tk.Button(buttonframe, text="3", font=('Arial, 18'))
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+
+btn4 = tk.Button(buttonframe, text="4", font=('Arial, 18'))
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
+
+btn5 = tk.Button(buttonframe, text="5", font=('Arial, 18'))
+btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
+
+btn6 = tk.Button(buttonframe, text="6", font=('Arial, 18'))
+btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
+
+buttonframe.pack(fill='x')
 
 root.mainloop()
 
