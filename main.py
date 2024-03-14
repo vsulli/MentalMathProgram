fdb212626e8d1a6ab67542df6f33302d557bdf3c
parosomniac <parosomniac@gmail.com>
2024-03-14 18:33:39 -0500

    creating a console version before graphical version

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+Mental Math Program
+Console-based program for solving mental math
+problems
+14 March 2024
+'''
+
+def main():
+    menu = ("1) Addition\n2) Subtraction\n" +  
+    "3) Multiplication\n4) Division\n5) Records")
+    # for records - print fastest for each category
+    print(menu)
+
+    
+
+main()
