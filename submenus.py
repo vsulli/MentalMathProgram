0c850ed39851c68a80a813374ef62ace1c0dfd83
parosomniac <parosomniac@gmail.com>
2024-03-14 19:21:18 -0500

    need to replace and split regex match

--- submenus.py
+++ submenus.py
@@ -3,11 +3,20 @@ import re
 # return a list
 def digit_sel()->list[int]:
     digits = []
-    sel = input('Select the number of digits for the ' +  
-                   'first and second number.ex) 1, 2')
+    prompt = input('Select the number of digits for the ' +  
+                   'first and second number.ex) 1, 2\n')
     
+    match = r'(\d+, \d+)' 
+    sel = re.findall(match, prompt)
+    if sel:
+        print(type(sel))
+       # sel.replace(" ", "")
+       # digits = sel.split(',')
+       # print(digits)
     return digits
     
 
 def addition(d1, d2):
     pass
+
+digit_sel()
