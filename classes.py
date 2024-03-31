74052582a3354ca771f84c03b3ad4a091ac3236e
parosomniac <parosomniac@gmail.com>
2024-03-31 16:03:21 -0500

    working to get percentage correct

---
+++ classes.py
@@ -0,0 +1,9 @@
+# all classes
+
+# user
+class User:
+    def __init__(self, username, correct, questions):
+        self.u = username
+        self.c = correct
+        self.q = questions
+
--- submenus.py
+++ submenus.py
@@ -1,9 +1,10 @@
+import classes
 import re
+import time
 
-#total questions practiced
-TOT_Q = 0
-# correct count
-CORRECT = 0
+# create function to create/ select user
+# load from file
+user = classes.User('parosomniac', 0, 0)
 
 # return a list
 def digit_sel()->list[int]:
@@ -24,20 +25,21 @@ def addition(d1, d2):
     prompt = ''
     while prompt.lower() != 'q':
         prompt = input(str(d1) + ' + ' + str(d2)+'\n')
-        global TOT_Q
-        TOT_Q += 1
+        user.q += 1
         match = r'\d+'
         sel = re.findall(match, prompt)
+        start_time = time.perf_counter()
         if prompt == 'q':
-            print('Qs: ' + str(TOT_Q)+'\n'+
-                  'Correct: ' + str(CORRECT)+'\n'+
-                  str(round(CORRECT/TOT_Q)*100, 2)+'%')
+            print('Qs: ' + str(user.q)+'\n'+
+                  'Correct: ' + str(user.c)+'\n'+
+                  str(round(user.c / user.q*100, 2))+'%')
             
             return
         elif sel[0]:
+            stop_time = time.perf_counter()
+            print(f"Time: {stop_time - start_time:0.4f} secs")
             if int(sel[0]) == d1 + d2:
-                global CORRECT
-                CORRECT += 1
+                user.c += 1
                 print('Correct\n')
             else:
                 print('Wrong\nCorrect Answer: ' + str(d1 + d2) + '\n')
