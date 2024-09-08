ba6469aaf1b493cc747dc141af94e8c75765840e
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-07 21:11:09 -0500

    shelve working to store array of user info

--- practice.py
+++ practice.py
@@ -5,13 +5,16 @@ from datetime import datetime, timedelta
 
 shelve_file = shelve.open("records2")
 
-# username: [[operation, fastest time], []]
-user_records = {'vsulli_add': [[1, 1, 5, '2024-09-06 20:52:30.462857'], [2, 1, 8, '2024-09-05 20:52:30.462857']], 
-'vsulli_sub': [[1, 1,9, '2024-09-04 20:49:44.771948'], [2, 1, 12, '2024-09-03 20:52:30.462857']],
-'vsulli_multi': [[1, 1, 2, '2024-09-02 20:52:30.462857' ], [2, 1,7, '2024-09-01 20:52:30.462857']],
-'vsulli_div': [[1, 1, 10, '2024-08-31 20:52:30.462857'], [2, 1, 25, '2024-08-30 20:52:30.462857']]}
+# username: [[digits_1, digits_2, fastest_time, date_achieved]]
+vsulli_add = [[1, 1, 5, '2024-09-06 20:52:30.462857'], [2, 1, 8, '2024-09-05 20:52:30.462857']]
+vsulli_sub = [[1, 1,9, '2024-09-04 20:49:44.771948'], [2, 1, 12, '2024-09-03 20:52:30.462857']]
+vsulli_multi =  [[1, 1, 2, '2024-09-02 20:52:30.462857' ], [2, 1,7, '2024-09-01 20:52:30.462857']]
+vsulli_div = [[1, 1, 10, '2024-08-31 20:52:30.462857'], [2, 1, 25, '2024-08-30 20:52:30.462857']]
 
-shelve_file['user_records'] = user_records
+shelve_file['vsulli_add'] = vsulli_add
+shelve_file['vsulli_sub'] = vsulli_sub
+shelve_file['vsulli_multi'] = vsulli_multi
+shelve_file['vsulli_div'] = vsulli_div
 
 shelve_file.close()
 
@@ -19,12 +22,13 @@ shelve_file.close()
 # retrieving data stored in the shelf file
 shelve_file = shelve.open('records2')
 
-print(user_records)
+
+print(f"Keys = {list(shelve_file.keys())}") 
 print('-------------------')
 
 # retrieve the hashmap
-user_records = shelve_file['user_records']
+vsulli_div = shelve_file['vsulli_div']
+print(vsulli_div)
 
 shelve_file.close()
 
-print(user_records)
